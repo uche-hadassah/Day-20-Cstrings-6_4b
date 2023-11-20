7c914f0f587b516e743f386f6e7fe6661d81ad8f
Hadassah Uche <uhadassah1@gmail.com>
2023-11-20 20:22:02 +0200

    Add project files.

---
+++ UcheHadassah6_4b/HadassahUche6_4b.cpp
@@ -0,0 +1,7 @@
+/*b) Re-write the above exercise with functions: Write a function called encrypt which 
+takes as a parameter a string and changes it to the encrypted version (as described 
+above). Write also a function called decrypt which takes as a parameter a string an 
+decrypts the string i.e. restores it to the original. For example decrypting the string s1
+containing “dbu” will convert it back to the string “cat”. (i.e. replace ‘d’ with ‘c’, ‘b’
+with ‘a’ and so on, thus reversing the coding scheme used for encryption above). 
+Write a main program to test both functions.*/
